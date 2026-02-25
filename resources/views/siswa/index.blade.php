bc5981bca93c1c4b865b33ebca5fd526e5ccea2a
drnnm1259-a11y <drnnm1259@gmail.com>
2026-02-25 10:42:17 +0700

    edit1

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -10,15 +10,29 @@
         <th>Kelas</th>
         <th>Jurusan</th>
         <th>Alamat</th>
+        <th>Aksi</th>
     </tr>
 
-    @foreach($siswas as $no => $siswa)
+    @forelse($siswas as $no => $siswa)
     <tr>
         <td>{{ $no + 1 }}</td>
         <td>{{ $siswa->nama }}</td>
         <td>{{ $siswa->kelas }}</td>
         <td>{{ $siswa->jurusan }}</td>
         <td>{{ $siswa->alamat }}</td>
+        <td>
+            <a href="{{ route('siswa.edit', $siswa->id) }}">Edit</a>
+
+            <form action="{{ route('siswa.destroy', $siswa->id) }}" method="POST" style="display:inline;">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Hapus</button>
+            </form>
+        </td>
     </tr>
-    @endforeach
+    @empty
+    <tr>
+        <td colspan="6">Belum ada data</td>
+    </tr>
+    @endforelse
 </table>
