48a9cf892902c246445ee731732d1d5b7dff1f19
extraJoss7 <duduldul479@gmail.com>
2026-02-25 10:25:29 +0700

    create

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Siswa extends Model
 {
-<<<<<<< HEAD
     protected $table = 'siswas';
 
     protected $fillable = [
@@ -16,7 +15,3 @@ class Siswa extends Model
         'alamat'
     ];
 }
-=======
-       protected $fillable = ['nama','kelas','jurusan','alamat'];
-}
->>>>>>> d31a848d7fecf10dd2f897ff2dfddc5e2296c2a7
