a2b767694dcdb5d2ff1754240a91a7507ec1e795
extraJoss7 <duduldul479@gmail.com>
2026-02-26 08:46:35 +0700

    perpustakaan dicoba

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\PerpustakaanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('siswa', SiswaController::class);
+Route::resource('perpustakaan', PerpustakaanController::class);
