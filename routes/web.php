92b8815c18eb2f2333fd05a1d46b29a0fa3b0783
drnnm1259-a11y <drnnm1259@gmail.com>
2026-02-27 08:16:34 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SiswaController;
-<<<<<<< HEAD
-=======
-use App\Http\Controllers\PerpustakaanController;
 use App\Http\Controllers\BukuController;
->>>>>>> caeca6c932c545dea5c325d4ec663ae61f5814c4
 
 Route::get('/', function () {
     return redirect('/login');
 });
 
-<<<<<<< HEAD
 /* AUTH */
 Route::get('/register', [AuthController::class, 'showRegister']);
 Route::post('/register', [AuthController::class, 'register']);
@@ -25,8 +20,5 @@ Route::post('/logout', [AuthController::class, 'logout']);
 
 /* SISWA (HARUS LOGIN) */
 Route::resource('siswa', SiswaController::class)->middleware('auth');
-=======
 Route::resource('siswa', SiswaController::class);
 Route::resource('buku', BukuController::class);
-Route::resource('perpustakaan', PerpustakaanController::class);
->>>>>>> caeca6c932c545dea5c325d4ec663ae61f5814c4
