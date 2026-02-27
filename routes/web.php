caeca6c932c545dea5c325d4ec663ae61f5814c4
extraJoss7 <duduldul479@gmail.com>
2026-02-27 08:11:37 +0700

    hapus

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
-<<<<<<< HEAD
-use App\Http\Controllers\BukuController;
-=======
 use App\Http\Controllers\PerpustakaanController;
->>>>>>> aba93074b2d33b7c3a471fda556377e768089159
+use App\Http\Controllers\BukuController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('siswa', SiswaController::class);
-<<<<<<< HEAD
 Route::resource('buku', BukuController::class);
-=======
 Route::resource('perpustakaan', PerpustakaanController::class);
->>>>>>> aba93074b2d33b7c3a471fda556377e768089159
